de249e41fad2c486eecb9e4e182f97c68eb874b0
Oliver <46686848+ORJackson@users.noreply.github.com>
2022-03-16 15:13:49 +0000

    added a comment

--- Restaurant.java
+++ Restaurant.java
@@ -2,7 +2,7 @@
 public class Restaurant {
     public Restaurant() {
     }
-
+//this is a comment 
     public static void main(String[] args) {
         Table table1 = new Table(1, 6);
         System.out.println(table1.getTableNumber());
