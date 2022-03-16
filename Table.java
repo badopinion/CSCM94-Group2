7fdd7608d91e608c80731ce3a9af560c77f26edd
Oliver <46686848+ORJackson@users.noreply.github.com>
2022-03-16 15:11:54 +0000

    class files changed to java

---
+++ Table.java
@@ -0,0 +1,25 @@
+public class Table {
+    private int tableNumber;
+    private int capacity;
+
+    public Table(int tableNumber, int capacity) {
+        this.tableNumber = tableNumber;
+        this.capacity = capacity;
+    }
+
+    public int getTableNumber() {
+        return this.tableNumber;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
