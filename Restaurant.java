2e1e7e9eb87bf082e2bc364df4c9ee673d5591e1
Oliver <46686848+ORJackson@users.noreply.github.com>
2022-03-16 15:06:13 +0000

    table and restaurant class

---
+++ Restaurant.java
@@ -0,0 +1,10 @@
+
+public class Restaurant {
+    public Restaurant() {
+    }
+
+    public static void main(String[] args) {
+        Table table1 = new Table(1, 6);
+        System.out.println(table1.getTableNumber());
+    }
+}
