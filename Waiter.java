3b436aad21ae217939053877a5de8be9d39cef3f
Oliver <46686848+ORJackson@users.noreply.github.com>
2022-03-17 15:53:10 +0000

    chef, driver, manager, waiter classes added

---
+++ Waiter.java
@@ -0,0 +1,7 @@
+
+public class Waiter extends Staff{
+
+    public Waiter(String firstName, String lastName, int staffId, String homeAddress){
+        super(firstName, lastName, staffId, homeAddress);
+    }
+}
