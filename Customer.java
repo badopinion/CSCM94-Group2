dc27e3cc6adf66c69c98909e044a8ef15e749cfc
SamRAINS99 <101827334+SamRAINS99@users.noreply.github.com>
2022-03-23 11:41:27 +0000

    customer java class

--- Customer.java
+++ Customer.java
@@ -1,10 +1,50 @@
+// This class holds information about the customer including (FirstName, LastName, Address, Order History) SR
 
-Public class Customer extends User{
 
-    private int customerId;
+import java.util.ArrayList;
 
-    public Customer(int customerId){
-        this.customerId = custoemrId;
+public class Customers {
+    String firstName;
+    String lastName;
+    String address;
+    ArrayList<Order> orderHist;
+
+    //
+
+    public Customers(String firstName, String lastName, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.orderHist = new ArrayList<Order>();
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ArrayList<Order> getOrderHist() {
+        return orderHist;
+    }
+
 
 }
