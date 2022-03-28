d65a2a3578ddf02c31209c0a2763cf0683804cf6
Yingfan Zhang <2105558@swansea.ac.uk>
2022-03-28 13:52:54 +0100

    evan-branch
    I'm not sure if I'm uploading to Branch the right way

---
+++ cafe94_group2/src/main/java/cafepackage/cafe94_group2/LoginController.java
@@ -0,0 +1,36 @@
+package cafepackage.cafe94_group2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+
+public class LoginController {
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private TextField enterUsernameField;
+
+
+
+    public void loginButtonOnAction(ActionEvent event){
+  
+    if(enterUsernameField.getText().isBlank()==false){
+        loginMessageLabel.setText("you are login");
+    }else{
+        loginMessageLabel.setText("Please enter username and password");
+    }
+    }
+
+    public void cancelButtonOnAction(ActionEvent event){
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+}
