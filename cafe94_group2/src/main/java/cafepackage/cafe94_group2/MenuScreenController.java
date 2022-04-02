29c9f13185936fa762c6971253442b4b8b328794
davidovake <davidova_ke@abv.bg>
2022-04-02 20:56:46 +0100

    food drink coffee button functionality

--- cafe94_group2/src/main/java/cafepackage/cafe94_group2/MenuScreenController.java
+++ cafe94_group2/src/main/java/cafepackage/cafe94_group2/MenuScreenController.java
@@ -6,6 +6,7 @@ import backend.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.event.ActionEvent;
 
@@ -13,11 +14,13 @@ import java.io.IOException;
 
 public class MenuScreenController {
     @FXML
-    Button coffee;
-    @FXML
-    Button goodbye;
-    @FXML
     TextArea display;
+    @FXML
+    Button food;
+    @FXML
+    Button coffebtn;
+    @FXML
+    Button drinksbtn;
 
     public void sayHello(ActionEvent event) throws IOException {
         String hello = "Hello";
@@ -26,7 +29,9 @@ public class MenuScreenController {
 
     public void sayGoodbye(ActionEvent event) throws IOException {
         // Get the restaurant from the file. This should work fine verbatim no matter where you call it.
-        Restaurant res = new Load().getRestaurantFromFile();
+//        Restaurant res = new Load().getRestaurantFromFile();
+        Load load = new Load();
+        Restaurant res = load.loadRestaurant();
 
         // Do stuff.
         if(res.menu.getAllItems().size() == 0) {
@@ -35,10 +40,91 @@ public class MenuScreenController {
         String hello = res.menu.returnMenuItems();
         display.setText(hello);
 
-        // Save the restaurant to file, so you don't lose your changes.
+        //Save the restaurant to file, so you don't lose your changes.
         res.saveRestaurant();
     }
 
+//
+//    public void displayFood(ActionEvent event)throws IOException{
+//        Restaurant res = new Load().getRestaurantFromFile();
+//        if(res.menu.getAllItems().size() == 0) {
+//            res.menu.populateMenu();
+//        }
+//        StringBuilder food = new StringBuilder();
+//        for(MenuItem menuItem: res.menu.returnMenuItemsByType(res.menu.getCurrentItems(), MenuItemType.FOOD)){
+//            food.append(menuItem.getName() + "\n" );
+//        }
+//        display.setText(String.valueOf(food));
+//
+//    }
+//
+//    public void displayCoffee(ActionEvent event)throws IOException{
+//        Restaurant res = new Load().getRestaurantFromFile();
+//        if(res.menu.getAllItems().size() == 0) {
+//            res.menu.populateMenu();
+//        }
+//        StringBuilder food = new StringBuilder();
+//        for(MenuItem menuItem: res.menu.returnMenuItemsByType(res.menu.getCurrentItems(), MenuItemType.COFFEE)){
+//            food.append(menuItem.getName() + "\n" );
+//        }
+//        display.setText(String.valueOf(food));
+//
+//    }
+//
+//    public void displayDrink(ActionEvent event)throws IOException{
+//        Restaurant res = new Load().getRestaurantFromFile();
+//        if(res.menu.getAllItems().size() == 0) {
+//            res.menu.populateMenu();
+//        }
+//        StringBuilder food = new StringBuilder();
+//        for(MenuItem menuItem: res.menu.returnMenuItemsByType(res.menu.getCurrentItems(), MenuItemType.DRINK)){
+//            food.append(menuItem.getName() + "\n" );
+//        }
+//        display.setText(String.valueOf(food));
+//
+//    }
+
+    public void displayFood(ActionEvent event)throws IOException{
+        Load load = new Load();
+        Restaurant res = load.loadRestaurant();
+        if(res.menu.getAllItems().size() == 0) {
+            res.menu.populateMenu();
+        }
+        StringBuilder food = new StringBuilder();
+        for(MenuItem menuItem: res.menu.returnMenuItemsByType(res.menu.getCurrentItems(), MenuItemType.FOOD)){
+            food.append(menuItem.getName() + "\n" );
+        }
+        display.setText(String.valueOf(food));
+
+    }
+
+    public void displayCoffee(ActionEvent event)throws IOException{
+        Load load = new Load();
+        Restaurant res = load.loadRestaurant();
+        if(res.menu.getAllItems().size() == 0) {
+            res.menu.populateMenu();
+        }
+        StringBuilder food = new StringBuilder();
+        for(MenuItem menuItem: res.menu.returnMenuItemsByType(res.menu.getCurrentItems(), MenuItemType.COFFEE)){
+            food.append(menuItem.getName() + "\n" );
+        }
+        display.setText(String.valueOf(food));
+
+    }
+
+    public void displayDrink(ActionEvent event)throws IOException{
+        Load load = new Load();
+        Restaurant res = load.loadRestaurant();
+        if(res.menu.getAllItems().size() == 0) {
+            res.menu.populateMenu();
+        }
+        StringBuilder food = new StringBuilder();
+        for(MenuItem menuItem: res.menu.returnMenuItemsByType(res.menu.getCurrentItems(), MenuItemType.DRINK)){
+            food.append(menuItem.getName() + "\n" );
+        }
+        display.setText(String.valueOf(food));
+
+    }
 
 
 }
