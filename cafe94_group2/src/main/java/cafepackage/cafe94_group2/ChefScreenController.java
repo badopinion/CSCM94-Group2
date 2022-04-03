60f3a40082db131c2c30162d35a8469ac85c2b9a
Yingfan Zhang <2105558@swansea.ac.uk>
2022-04-03 22:09:49 +0100

    change

--- cafe94_group2/src/main/java/cafepackage/cafe94_group2/ChefScreenController.java
+++ cafe94_group2/src/main/java/cafepackage/cafe94_group2/ChefScreenController.java
@@ -1,6 +1,31 @@
 package cafepackage.cafe94_group2;
 
 import backend.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class ChefScreenController {
+
+    @FXML
+    private Button addItemButton;
+    public void AddItemOnAction(ActionEvent event) throws IOException {
+        Stage chefAddScreen = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("ChefAddScreen.fxml"));
+        chefAddScreen.setTitle("Chef Add Interface");
+        chefAddScreen.setScene(new Scene(root, 730, 530));
+        chefAddScreen.setX(600);
+        chefAddScreen.setY(250);
+        chefAddScreen.initModality(Modality.APPLICATION_MODAL);
+        chefAddScreen.show();
+
+    }
 }
