bf3eb54ce8fb7e25ee81e3d037936593935206d2
Yingfan Zhang <2105558@swansea.ac.uk>
2022-04-05 00:45:00 +0100

    change about javadoc in back-end

--- cafe94_group2/src/main/java/backend/Waiter.java
+++ cafe94_group2/src/main/java/backend/Waiter.java
@@ -1,8 +1,19 @@
 package backend;
 
+/**
+ * This is Constructor
+ * @author Jo Butler
+ * @version 1.0
+ */
 public class Waiter extends Staff{
-
-	// Constructor - JB
+    /**
+     * This is waiter information
+     * @param username The username of waiter
+     * @param password The password of waiter
+     * @param firstName First name of waiter
+     * @param lastName Last name of waiter
+     * @param homeAddress Home address of waiter
+     */
     public Waiter(String username, String password, String firstName, String lastName, String homeAddress){
         super(username, password, firstName, lastName, homeAddress);
     }
