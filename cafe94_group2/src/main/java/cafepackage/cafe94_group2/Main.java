423de1575e4ebba77d47480e662565cff1cfb081
Yingfan Zhang <2105558@swansea.ac.uk>
2022-04-04 17:38:41 +0100

    change

--- cafe94_group2/src/main/java/cafepackage/cafe94_group2/Main.java
+++ cafe94_group2/src/main/java/cafepackage/cafe94_group2/Main.java
@@ -9,7 +9,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.*;
 
+/**
+ * This class serves as a driver class and launches the GUI display.
+ * @author Yingfan Zhang
+ * @version 1.0
+ */
 public class Main extends Application {
+    /**
+     * This method sets and displays the main stage/screen.
+     * @param primaryStage is the main screen of the program
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Main.class.getResource("Login.fxml"));
@@ -18,6 +27,10 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * This method serves as the main method and launches the program.
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
 		// Launch JavaFX application.
         launch();
